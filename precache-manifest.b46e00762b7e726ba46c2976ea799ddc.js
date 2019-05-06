6ce22e2e7d699ba78c8485fcdda4efba1e39180a
Tan Li Hau <lhtan93@gmail.com>
2019-05-07 01:09:47 +0800

    Built on Tue May 07 2019 01:09:47 GMT+0800 (+08)

---
+++ precache-manifest.b46e00762b7e726ba46c2976ea799ddc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8431617961e5ea83841c5eb283b17dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "647d9965abe70ad98cdf",
+    "url": "/static/css/main.10c25449.chunk.css"
+  },
+  {
+    "revision": "be746d8b1fcf4884b002",
+    "url": "/static/js/2.4620e2fc.chunk.js"
+  },
+  {
+    "revision": "647d9965abe70ad98cdf",
+    "url": "/static/js/main.0cf1acd8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/utils/ContextObject.js
+++
@@ -1,9 +0,0 @@
-export default class ContextObject {
-  constructor(name, value) {
-    this.name = name;
-    this.value = value;
-  }
-  setValue(value) {
-    return new ContextObject(this.name, value);
-  }
-}
